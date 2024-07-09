ecfdaab40ec31866c553ac2271e7af665f999f3f
JacobitKashala <Jacobitkashala@gmail.com>
2024-07-09 08:16:27 +0100

    config

--- config/Database.php
+++ config/Database.php
@@ -15,7 +15,7 @@ class Database{
         $this->connexion = null;
 
         try{
-            $this->connexion = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->connexion = new PDO("mysql:host=" . $this->host . "dbname=" . $this->db_name, $this->username, $this->password);
             $this->connexion->exec("set names utf8");
         }catch(PDOException $exception){
             echo "Erreur de connexion : " . $exception->getMessage();
